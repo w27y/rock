a02188ce66b3f95d91d17e3179112f01eb4152ac
w27y <mike@mikeshehata.com>
2020-11-25 21:39:31 -0800

    Version 1 Complete

--- logic.js
+++ logic.js
@@ -1,2 +1,62 @@
-const rockButton = document.getElementById('rockButton');
-rockButton.addEventListener
+/*
+Author: Mike Shehata
+Date:  11/25/2020
+Version: 1.0
+*/
+
+main();
+
+//These are the controls of the game
+function main () {
+    let roundNumber = 0;
+    const rockButton = document.getElementById('rockButton');
+    rockButton.addEventListener('click',function(){
+    showRound(roundNumber += 1);
+    showMessage("Rock");
+    cpuLogic('Rock');
+    });
+    const paperButton = document.getElementById('paperButton');
+    paperButton.addEventListener('click',function(){
+    showRound(roundNumber += 1);
+    showMessage("Paper");
+    cpuLogic('Paper');   
+    });
+    const scissorsButton = document.getElementById('scissorsButton');
+    scissorsButton.addEventListener('click',function(){
+    showRound(roundNumber += 1);
+    showMessage("Scissors");
+    cpuLogic('Scissors');  
+    });
+}
+
+//Logic that compares user vs CPU options
+function cpuLogic(userOption) {
+    const cpuArray = Array('Rock', 'Paper', 'Scissors');
+    const random = Math.floor(Math.random() * cpuArray.length);
+    showCpu(cpuArray[random]);
+    let userScore = 0;
+    if (userOption === cpuArray[random]) {
+        showGame("Its a Tie!!!");
+    }
+    else if (userOption === "Rock" && cpuArray[random] === "Paper") {
+        userLose();
+    }
+    else if (userOption === "Rock" && cpuArray[random] === "Scissors") {
+        userWin();
+    }
+    else if (userOption === "Paper" && cpuArray[random] === "Scissors") {
+        userLose();
+    }
+    else if (userOption === "Paper" && cpuArray[random] === "Rock") {
+        userWin();
+    }
+    else if (userOption === "Scissors" && cpuArray[random] === "Rock") {
+        userLose();
+    }
+    else if (userOption === "Scissors" && cpuArray[random] === "Paper") {
+        userWin();
+    }
+    else {
+        showGame(null);
+    }
+}
--- util.js
+++ util.js
@@ -1,3 +1,31 @@
-function showMessage(result) {
-    document.getElementById('result').textContent = result;
+let userScore = 0;
+
+function showMessage(userResult) {
+    document.getElementById('userResult').textContent = userResult;
+}
+
+function showCpu(cpuResult) {
+    document.getElementById('cpuResult').textContent = cpuResult;
+}
+
+function showGame(gameResult) {
+    document.getElementById('gameResult').textContent = gameResult;
+}
+
+function showRound(round) {
+    document.getElementById('round').textContent = round;
+}
+
+function showScore(score){
+    document.getElementById('score').textContent = score;
+}
+
+function userLose(){
+    showGame("You Lose!!!");
+}
+
+function userWin(){
+    showGame("You Win!!!");
+    userScore += 1;
+    showScore(userScore);
 }
